6e1c571e3b9c4ad592ca0e73f192c5793d91e7f0
akhlakm <amahmoo3@ncsu.edu>
2023-02-24 12:08:39 -0500

    Add random forest regressor

--- x5_train_regressor.py
+++ x5_train_regressor.py
@@ -5,6 +5,7 @@ from importlib import reload
 import numpy as np
 import pandas as pd
 
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process import kernels
 
@@ -13,6 +14,13 @@ from model import regressor, features
 #%% Training data
 train = pd.read_csv("Data/imputed_data.mice.csv")
 train.head()
+
+dfi = train[train["imp"] != 0]
+df1 = train[train["imp"] == 1]
+df2 = train[train["imp"] == 2]
+df3 = train[train["imp"] == 3]
+df4 = train[train["imp"] == 4]
+df5 = train[train["imp"] == 5]
 
 # Specify the features and target
 xcols = ['lspk1', 'tspk1', 'lsfw1', 'tsfw1', 'lspk2', 'tspk2',
@@ -23,12 +31,9 @@ ycols = ['lobefrac', 'fullfrac']
 #%% Prepare for training with the imputated dataset 1
 reload(regressor)
 
-df1 = train[train["imp"] == 1]
-df2 = train[train["imp"] == 2]
-
-ml = regressor.Regressor(df1)
-ml.AddFeatures(features.Differences)
-ml.SetColumns(xcols, 'fullfrac')
+ml = regressor.Regressor(dfi)
+ml.AddFeatures(features.Differences, features.InverseDifferences)
+ml.SetColumns(xcols, 'lobefrac')
 
 # %% Gaussian Process Regression
 kern = kernels.RBF() + kernels.WhiteKernel()
@@ -36,4 +41,26 @@ gpr = GaussianProcessRegressor(kernel=kern)
 ml.FitModel(gpr)
 
 # %% Fitness
+ml.Fitness(ml.PrepPrediction(df1))
 ml.Fitness(ml.PrepPrediction(df2))
+ml.Fitness(ml.PrepPrediction(df3))
+ml.Fitness(ml.PrepPrediction(df4))
+ml.Fitness(ml.PrepPrediction(df5))
+
+# %% Random Forest Regressor
+ml2 = regressor.Regressor(dfi)
+ml2.AddFeatures(features.Differences, features.InverseDifferences)
+ml2.SetColumns(xcols, 'lobefrac')
+
+rfr = RandomForestRegressor(n_estimators=100, random_state=42)
+
+ml2.FitModel(rfr)
+
+# %% Fitness
+ml2.Fitness(ml2.PrepPrediction(df1))
+ml2.Fitness(ml2.PrepPrediction(df2))
+ml2.Fitness(ml2.PrepPrediction(df3))
+ml2.Fitness(ml2.PrepPrediction(df4))
+ml2.Fitness(ml2.PrepPrediction(df5))
+
+# %%

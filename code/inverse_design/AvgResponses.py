569ea5a49c1baa3f6b9a4d46331fe2587ac41e92
akhlakm <me@akhlakm.com>
2023-12-05 19:14:49 -0500

    Add new files

--- code/inverse_design/AvgResponses.py
+++ code/inverse_design/AvgResponses.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-import os
-from re import A
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -9,8 +7,6 @@ import features
 import response
 
 import xgboost as xgb
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process import kernels
 
 plt.style.use("../matplotlib.mplstyle")
 mice = pd.read_csv("../../Data/imputed_data.mice.csv")
@@ -20,23 +16,8 @@ xcols = ['lspk1', 'tspk1', 'lsfw1', 'tsfw1', 'lspk2', 'tspk2',
          'lsfw2', 'tsfw2', 'lspk3', 'tspk3', 'lsfw3', 'tsfw3']
 ycols = ['full', 'other', 'lobe']
 
-# Gold
-x1 = ['lspk1', 'tspk1', 'lsfw1', 'tsfw1', # peaks, fwhm
-      'dp11', 'dw11', # shift, broadening
-     ]
-
-# Coating
-x2 = ['teos', 'lspk2','tspk2', 'lsfw2', 'tsfw2', # peaks, fwhm
-      'lp21', 'tp21', 'lw21', 'tw21', 'dp22', 'dp21', 'dw22', 'dw21', # shift, broadening
-     ]
-
-# Purification
-x3 = ['lspk3', 'tspk3','lsfw3', 'tsfw3', # peaks, fwhm
-      'lp31', 'lp32', 'tp31', 'tp32', 'lw31', 'lw32', 'tw31', 'tw32', 'dp33', 'dp31', 'dp32', 'dw33', 'dw31', 'dw32', # shift, broadening
-     ]
-
-bestfeats3 = ['tspk1', 'tsfw3', 'lsfw2', 'lspk1', 'tspk2', 'lspk2', 'dw11', 'tp31', 'dp32']
-bestfeats2 = ['tspk1', 'lspk1', 'lsfw2', 'tw21', 'dw11', 'tspk2', 'tsfw2', 'lspk2', 'tsfw1']
+bestfeats = ['teosVolume', 'teosVolPct', 'tsfw2', 'lsfw3', 'tsfw1', 'lsfw2', 'tspk2',
+       'lspk2', 'tspk1', 'lspk1', 'lsfw1', 'lspk3', 'tsfw3', 'tspk3']
 
 xgparams = dict(base_score=0.5, booster='gbtree', callbacks=None,
     colsample_bylevel=1, colsample_bynode=1, colsample_bytree=1,
@@ -51,18 +32,9 @@ xgparams = dict(base_score=0.5, booster='gbtree', callbacks=None,
 
 def BuildXGBModel(y, alg, df):
     ml = regressor.Regressor(df)
-    ml.Augment(xcols+ycols, K=4, scale=0.10, plot=False)
+    ml.Augment(xcols+ycols, K=2, scale=0.3, plot=False)
     ml.AddFeatures(features.Differences, show_list=False)
-    ml.Fit(bestfeats3, y, alg, plot=False)
-    return ml
-
-kern = kernels.RBF() + kernels.WhiteKernel()
-
-def BuildGPRModel(y, alg, df):
-    ml = regressor.Regressor(df)
-    ml.Augment(xcols+ycols, K=3, scale=0.10, plot=False)
-    ml.AddFeatures(features.Differences, show_list=False)
-    ml.Fit(bestfeats3, y, alg, plot=False)
+    ml.Fit(bestfeats, y, alg, plot=False)
     return ml
 
 allres = {
@@ -94,29 +66,6 @@ for imp in range(1, 6):
             allres[col].append(res[col].values[0])
         allres['imp'].append(imp)
         allres['alg'].append('xgb')
-        allres['opt'].append(opt)
-        print(res)
-
-        # GPR Regression Models
-        # =============================================================
-        gpmdl1 = BuildGPRModel("lobe", GaussianProcessRegressor(kernel=kern), df)
-        gpmdl2 = BuildGPRModel("full", GaussianProcessRegressor(kernel=kern), df)
-        gpmdl3 = BuildGPRModel("other", GaussianProcessRegressor(kernel=kern), df)
-
-        x0 = gpmdl1.ScaleX(gpmdl1.Prep(df)).mean()
-        res = response.GAOptimize(x0, 
-                        [gpmdl1, response.MaximizeNorm if opt == "lobe" else response.MinimizeNorm],
-                        [gpmdl2, response.MaximizeNorm if opt == "full" else response.MinimizeNorm],
-                        [gpmdl3, response.MinimizeNorm]
-        )
-
-        res = xgmdl1.UnscaleX(xgmdl1.Prep(res[0]))
-        for col in res:
-            if col not in allres:
-                allres[col] = []
-            allres[col].append(res[col].values[0])
-        allres['imp'].append(imp)
-        allres['alg'].append('gpr')
         allres['opt'].append(opt)
         print(res)
 
---
+++ code/model/AdRegress.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import r2_score, mean_squared_error
+import pipeline
+
+
+class TestPerformance(pipeline.Adapter):
+    """ Test the performance of a regressor. """
+    def __init__(self, savePref=None, show=True, use_validation = False, use_test = True):
+        self.savePref = savePref
+        self.show = show
+        self.useValidationSet = use_validation
+
+    def _plot_parity(self, y, p, pl):
+        R2 = r2_score(y, p)
+        RMSE = mean_squared_error(y, p, squared=False)
+        
+        fig, ax = plt.subplots(1, 1, figsize=(3.25, 2.5))
+        ax.plot(y, p, 'rx')
+        dline = np.linspace(*ax.get_xlim())
+        ax.plot(dline, dline, 'k--')
+        ax.set_xlabel("Truth")
+        ax.set_ylabel("%s Prediction" %pipeline.nice_name(pl.model))
+        ax.set_title("R2 = %0.2f, RMSE = %0.2f" %(R2, RMSE))
+
+        plt.tight_layout()
+        
+        if self.savePref:
+            path = "%s.pipeline%02d.png" %(self.savePref, self.lineId)
+            plt.savefig(path)
+            print("Save OK:", path)
+        
+        if self.show:
+            plt.show()
+        else:
+            plt.close()
+            
+    def _calc_score(self, y, p, pl):
+        R2 = r2_score(y, p)
+        RMSE = mean_squared_error(y, p, squared=False)
+
+        # data frame of the results
+        pl.score_report = None
+        pl.score = R2
+        
+        if self.useValidationSet:            
+            pl.stats['val_r2'] = R2
+            pl.stats['val_rmse'] = RMSE
+        else:
+            pl.stats['r2'] = R2
+            pl.stats['rmse'] = RMSE
+
+        return pl
+
+    def Process(self, pl):
+        if self.useValidationSet:
+            assert pl.Tv is not None, "Validation set not defined"
+            X = pl.Tv[pl.xCols]
+            y = pl.Tv[pl.yCol]
+            self.sayf("Run on validation dataset.")
+        else:
+            assert pl.Ts is not None, "Test set not defined"
+            X = pl.Ts[pl.xCols] # use the test set
+            y = pl.Ts[pl.yCol]
+
+        p = pl.model.predict(X)
+        pl = self._calc_score(y, p, pl)
+        self.sayf("R2 SCORE: {}", pl.score)
+        self._plot_parity(y, p, pl)
+        return pl
